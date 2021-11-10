34a9c99247ace96bef92e1d8991551b856776601
Jan Cimbalnik <jan.cimbalnik@fnusa.cz>
2021-11-10 17:58:06 +0100

    version change

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ DREAD_EXT = Extension("pydread",
                       include_dirs=["pydread/dlib"])
 
 setup(name = "pydread",
-      version='0.0b9',
+      version='0.0a1',
       description='Wrapper of C library for .d file reading',
       url='https://github.com/ICRC-BME/pydread',
       author='Jan Cimbalnik',
